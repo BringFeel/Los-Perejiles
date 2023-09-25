dffb4bf3e3effb065042a8dca24b7a602d370c3f
BringFeel <69062690+BringFeel@users.noreply.github.com>
2023-09-25 01:24:49 -0300

    Un poco menos horrible lo extremadamente horrible

---
+++ assets/js/copy-ip.js
@@ -0,0 +1,82 @@
+//Ya se que el código es horrible. Pueden cuestionar mis métodos pero no mis resultados
+
+      function copy(that){
+				var inp =document.createElement('input');
+				document.body.appendChild(inp)
+				inp.value = "cs.losperejiles.com.ar:27060";
+				inp.select();
+				document.execCommand('copy',false);
+				inp.remove();
+			}
+			
+			function copy2(that){
+				var inp =document.createElement('input');
+				document.body.appendChild(inp)
+				inp.value = "cs.losperejiles.com.ar:27050";
+				inp.select();
+				document.execCommand('copy',false);
+				inp.remove();
+			}
+			
+			function copy3(that){
+				var inp =document.createElement('input');
+				document.body.appendChild(inp)
+				inp.value = "cs.losperejiles.com.ar:27040";
+				inp.select();
+				document.execCommand('copy',false);
+				inp.remove();
+			}
+			
+			function copy4(that){
+				var inp =document.createElement('input');
+				document.body.appendChild(inp)
+				inp.value = "cs.losperejiles.com.ar:27030";
+				inp.select();
+				document.execCommand('copy',false);
+				inp.remove();
+			}
+			
+			function copy5(that){
+				var inp =document.createElement('input');
+				document.body.appendChild(inp)
+				inp.value = "cs.losperejiles.com.ar:27020";
+				inp.select();
+				document.execCommand('copy',false);
+				inp.remove();
+			}
+			
+			function copy6(that){
+				var inp =document.createElement('input');
+				document.body.appendChild(inp)
+				inp.value = "cs.losperejiles.com.ar:27010";
+				inp.select();
+				document.execCommand('copy',false);
+				inp.remove();
+			}
+	
+	                function copy7(that){
+				var inp =document.createElement('input');
+				document.body.appendChild(inp)
+				inp.value = "crimson.losperejiles.com.ar";
+				inp.select();
+				document.execCommand('copy',false);
+				inp.remove();
+			}
+
+		        function copy8(that){
+				var inp =document.createElement('input');
+				document.body.appendChild(inp)
+				inp.value = "sentry.losperejiles.com.ar";
+				inp.select();
+				document.execCommand('copy',false);
+				inp.remove();
+			}
+
+	                function copy10(that){
+				var inp =document.createElement('input');
+				document.body.appendChild(inp)
+				inp.value = "cs.losperejiles.com.ar:27070";
+				inp.select();
+				document.execCommand('copy',false);
+				inp.remove();
+			}
